56acbcba0eb275213b0db2ec349431ba70c64949
labpak <43479201+labpak@users.noreply.github.com>
2020-10-14 20:11:26 +0300

    R

---
+++ ConvexHull/ConvexHull/AndrewsAlgorithm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace ConvexHull
+{
+    static class AndrewsAlgorithm
+    {
+         public static double cross(Point O, Point A, Point B)
+        {
+            return (A.X - O.X) * (B.Y - O.Y) - (A.Y - O.Y) * (B.X - O.X);
+        }
+
+        public static List<Point> ConvexHull(List<Point> points)
+        {
+            if (points == null)
+                return null;
+
+            if (points.Count() <= 1)
+                return points;
+
+            int n = points.Count(), k = 0;
+            List<Point> hull = new List<Point>(new Point[2 * n]);
+
+            points.Sort((a, b) => a.X == b.X ? a.Y.CompareTo(b.Y) : a.X.CompareTo(b.X));
+
+            //lower
+            for (int i = 0; i < n; ++i)
+            {
+                while (k >= 2 && cross(hull[k - 2], hull[k - 1], points[i]) <= 0)
+                    k--;
+                hull[k++] = points[i];
+            }
+
+            //upper
+            for (int i = n - 2, t = k + 1; i >= 0; i--)
+            {
+                while (k >= t && cross(hull[k - 2], hull[k - 1], points[i]) <= 0)
+                    k--;
+                hull[k++] = points[i];
+            }
+
+            return hull.Take(k - 1).ToList();
+        }      
+    }
+}
---
+++ ConvexHull/ConvexHull/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace ConvexHull
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Point> m_Points = new List<Point>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_Points = new List<Point>();
+            this.Invalidate();
+        }
+
+        private void Form1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            m_Points.Add(new Point(e.X, e.Y));
+            this.Invalidate();
+        }
+        
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(this.BackColor);
+            List<Point> hull = null;
+            if (m_Points.Count >= 3)
+                hull = AndrewsAlgorithm.ConvexHull(m_Points);               
+
+            foreach (Point pt in m_Points)
+                e.Graphics.DrawEllipse(Pens.Black, pt.X - 3, pt.Y - 3, 6, 6);
+
+            if (m_Points.Count >= 3)
+            {
+                Point[] hull_points = new Point[hull.Count];
+                hull.CopyTo(hull_points);
+                e.Graphics.DrawPolygon(Pens.Blue, hull_points);
+            }
+        }
+    }
+}
